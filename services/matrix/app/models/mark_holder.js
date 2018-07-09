30aebd5b8367346be68dafbee10fdcd7fecc74f8
Sergey <serg.kukuev@yandex.ru>
2018-07-09 16:45:37 +0300

    #1 Подкорректирована модель

--- services/matrix/app/models/mark_holder.js
+++ services/matrix/app/models/mark_holder.js
@@ -24,43 +24,89 @@ MarkHolder.virtual('date').get(function() {
 });
 
 MarkHolder.statics.create = function(obj, callback) {
-	return obj.save(callback);
+	return obj.save(function(err, obj) {
+		err ? callback(err, null) : callback(null, Format(obj));
+	});
 }
 
-MarkHolder.statics.read = function(id, callback) {
+MarkHolder.statics.get = function(page = 0, count = 0, callback) {
+	if (count == 0) {
+		return this.find(function(err, users) {
+			if (err)
+				callback(err, null);
+			else {
+				if (users) {
+					let result = [];
+					for (let i = 0; i < users.length; i++)
+						result[i] = Format(users[i]);
+					callback(null, result);
+				}
+				else
+					callback(null, null);
+			}	
+		});
+	}
+	else {
+		return this.find(function(err, users) {
+			if (err)
+				callback(err, null);
+			else {
+				if (users) {
+					let result = [];
+					for (let i = 0; i < users.length; i++)
+						result[i] = Format(users[i]);
+					callback(null, result);
+				}
+				else
+					callback(null, null);
+			}	
+		}).skip(page * count).limit(count);
+	}
+}
+
+MarkHolder.statics.getById = function(id, callback) {
 	return this.find({ id_user: id }, function(err, obj) {
-		err ? callback(err, null) : (obj ? callback(null, getUserMarks(obj)) : callback(null, null));
+		err ? callback(err, null) : (obj ? callback(null, Format(obj)) : callback(null, null));
 	});
 }
 
-MarkHolder.statics.update = function(data, callback) {
-	return this.findAndUpdate({ id_user: data.id }, { 
-		id_user: data.id, 
-		test: data.test
-	}, { new: true }, function(err, obj) {
-		err ? callback(err, null) : (obj ? callback(null, getUserMarks(obj)) : callback(null, null));
+MarkHolder.statics.updateById = function(data, callback) {
+	return this.findAndUpdate({ id_user: data.id }, { test: data.test }, { new: true }, function(err, obj) {
+		err ? callback(err, null) : (obj ? callback(null, Format(obj)) : callback(null, null));
 	});
 }
 
-MarkHolder.statics.delete = function(id, callback) {
+MarkHolder.statics.deleteById = function(id, callback) {
 	return this.findAndRemove({ id_user: id }, function(err, obj) {
-		err ? callback(err, null) : (obj ? callback(null, getUserMarks(obj)) : callback(null, null));
+		err ? callback(err, null) : (obj ? callback(null, Format(obj)) : callback(null, null));
 	});
 }
 
-MarkHolder.statics.clear = function(callback) {
+MarkHolder.statics.delete = function(callback) {
 	return this.remove({}, function(err, result) {
 		err ? callback(err, null) : (result ? callback(null, result) : callback(null, null));
 	});
 }
 
 // Формирование JSON объекта
-function getUserMarks(obj) {
+function Format(obj) {
+	let test = [];
+	for (let i = 0; i < obj.test.length; i++) {
+		let temp = {
+			id_knowledge: obj.test[i].id_knowledge,
+			mark: obj.test[i].mark
+		}
+		test.push(temp);
+	}
 	let marks = {
-		"id": obj.id_user,
-		"data": obj.test
+		id: obj.id_user,
+		test: test
 	}
 	return marks;
+}
+
+MarkHolder.statics.Format = function(user) {
+	return Format(user);
 }
 
 mongoose.model("MarkHolder", MarkHolder);
