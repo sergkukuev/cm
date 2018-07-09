00f7993ca0a584292454b075f802d3556d9581d2
Sergey <serg.kukuev@yandex.ru>
2018-07-09 15:15:58 +0300

    #1 Добавлены новые статусы

--- services/matrix/app/validators/status.js
+++ services/matrix/app/validators/status.js
@@ -21,5 +21,28 @@ module.exports = {
     },
     ObjDeleted : function(id) {
         return "Объект с идентификатором(" + id + ") удален";
+    },
+    ObjArray : function(name) {
+        return "Параметр " + name + " должен быть массивом";
+    },
+    SzEqual : function() {
+        let res = "Размер следующих массивов должен быть одинаковый (";
+        for (let i = 0; i < arguments.length; i++) {
+            let dot = (arguments.length - 1 == i) ? "\')" : "\',";
+            res = res + "\'" + arguments[i] + dot;
+        }
+        return res;
+    },
+    SzKnowledge : function() {
+        let res = "Количество требуемых знаний не совпадает. Проверьте следующие параметры (";
+        for (let i = 0; i < arguments.length; i++) {
+            let dot = (arguments.length - 1 == i) ? "\')" : "\',";
+            res = res + "\'" + arguments[i] + dot;
+        }
+        res[res.length - 2] = ")";
+        return res;
+    },
+    incorrectData : function(name) {
+        return "Данные параметра \'" + name +"\' являются некорректными или отсутствуют";
     }
 }
