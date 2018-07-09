214e1cb557550133b4b640e7f5f8e0a621d9f5b1
Sergey <serg.kukuev@yandex.ru>
2018-07-09 11:17:31 +0300

    #1 Перемещен файл форматов JSON

--- services/matrix/app/validators/format.js
+++ services/matrix/app/validators/format.js
@@ -3,15 +3,31 @@ module.exports = {
     // Шаблон для отправки данных
     // (для универсальности, в случае добавления новых полей, к примеру токенов для межсервисной авторизации)
     Data : function(data) {
-    let item = {
-
-    };
-    return data;
+        return data;
     },
     // Шаблон для отправки статуса
-    Status : function(st, desc) {
+    Status : function(st, cd, desc) {
         let item = {
             status: st,
+            code: cd,
+            description: desc
+        };
+        return item;
+    },
+    // Клиентская ошибка
+    BadRequest : function(desc) {
+        let item = {
+            status: "Error",
+            code: 400,
+            description: desc
+        };
+        return item;
+    },
+    // Ресурс не найден
+    NotFound : function(desc) {
+        let item = {
+            status: "Error",
+            code: 404,
             description: desc
         };
         return item;
