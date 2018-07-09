cb423bed0a386be75387bf57155c6200f24fa764
Sergey <serg.kukuev@yandex.ru>
2018-07-09 15:14:42 +0300

    #1 Изменена модель работ

--- services/matrix/app/models/work.js
+++ services/matrix/app/models/work.js
@@ -8,8 +8,33 @@ let Work = new Schema({
         type: String, 
         required: true
     },
-    // Список идентификаторов задач для данной работы
-    tasks: [Schema.Types.ObjectId]
+    // Требуемые задачи для даной работы
+    tasks: [{
+        // Наименование задачи
+        name: {	
+            type: String,
+            required: true
+        },
+        // Ранг текущей задачи (0 - ведущий, 1 - 1кат, 2 - 2кат, 3 - 3кат, 4 - без кат)
+        rank: { 
+            type: Number,
+            min: 0,
+            max: 4,
+            default: 3
+        },
+        // Требуемые знания для выполнения задачи
+        need: [{
+            // ID знания
+            id_knowledge: Schema.Types.ObjectId, 
+            // Необходимый уровень знаний (0 - знание не требуется, 4 - необходим уровень эксперта)
+            mark: {
+                type: Number,
+                min: 1,
+                max: 4, 
+                required: true
+            }
+        }]
+    }]
 });
 
 Work.virtual('date').get(function() {
@@ -22,7 +47,7 @@ Work.statics.create = function(work, callback) {
 	});
 }
 
-Work.statics.read = function(page = 0, count = 0, callback) {
+Work.statics.get = function(page, count, callback) {
     if (count == 0) {
         return this.find(function(err, work) {
             if (err)
@@ -57,13 +82,13 @@ Work.statics.read = function(page = 0, count = 0, callback) {
     }
 }
 
-Work.statics.read =  function(id, callback) {
+Work.statics.getById =  function(id, callback) {
     return this.findById(id, function(err, work) {
         err ? callback(err, null) : (work ? callback(null, Format(work)) : callback(null, null));
     });
 }
 
-Work.statics.update = function(id, data, callback) {
+Work.statics.updateById = function(id, data, callback) {
     return this.findByIdAndUpdate(id, { 
         name: data.name, 
         tasks: data.tasks 
@@ -72,25 +97,48 @@ Work.statics.update = function(id, data, callback) {
     });
 }
 
-Work.statics.delete = function(id, callback) {
+Work.statics.deleteById = function(id, callback) {
 	return this.findByIdAndRemove(id, function(err, work) {
-		err ? callback(err, null) : (obj ? callback(null, Format(work)) : callback(null, null));
+		err ? callback(err, null) : (work ? callback(null, Format(work)) : callback(null, null));
 	});
 }
 
-Work.statics.clear = function(callback) {
+Work.statics.delete = function(callback) {
 	return this.remove({}, function(err, result) {
 		err ? callback(err, null) : (result ? callback(null, result) : callback(null, null));
 	});
 }
 
 function Format(work) {
+    let tasks = [];
+    for (let i = 0; i < work.tasks.length; i++) {
+        let kn_info = [];
+        for (let j = 0; j < work.tasks[i].need.length; j++) {
+            let temp = {
+                id_knowledge: work.tasks[i].need[j].id_knowledge,
+                mark: work.tasks[i].need[j].mark
+            }
+            kn_info.push(temp);
+        }
+        let task = {
+            id: work.tasks[i]._id,
+            name: work.tasks[i].name,
+            rank: work.tasks[i].rank,
+            need: kn_info
+        };
+        tasks.push(task);
+    }
+    
     let item = {
         id: work._id,
         name: work.name,
-        id_tasks: work.tasks
-    }
+        tasks: tasks
+    };
     return item;
+}
+
+Work.statics.Format = function(work) {
+    return Format(work);
 }
 
 mongoose.model("Work", Work);
