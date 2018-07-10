eeeb03c5c0f2a576c50e72e97dd61d8cf1ca69b6
Sergey <serg.kukuev@yandex.ru>
2018-07-10 14:14:35 +0300

    Удален код из JSON ошибки

--- services/aggregation/app/coordinators/requester.js
+++ services/aggregation/app/coordinators/requester.js
@@ -36,7 +36,7 @@ module.exports = {
     Response : function(err, status, response, callback) {
         if (err) {
             if (err.code == "ECONNREFUSED") 
-                return callback(err, 503, { status: "Error", code: 503, description: "Сервер недоступен, повторите попытку позже" });
+                return callback(err, 503, { status: "Error", description: "Сервер недоступен, повторите попытку позже" });
             return callback(err, status, JSON.parse(response));
         }
         else {
