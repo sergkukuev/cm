304012c2a22e1ccb56f6a1aa5acf27ea804d813b
Sergey <serg.kukuev@yandex.ru>
2018-07-10 10:44:45 +0300

    Добален модуль обработки запросов

---
+++ services/aggregation/app/coordinators/requester.js
@@ -0,0 +1,50 @@
+// Модуль обработки запросов
+module.exports = {
+    Options : function(uri, method) {
+        let item = {
+            method: method, 
+            uri: uri
+        };
+        return item;
+    },
+    HttpHead : function(opt, callback) {
+        const req = require('request');
+        req.head(opt.uri, opt, function(err, res, body) {
+            err ? callback(err, null, null) : callback(null, res.statusCode, body);
+        });
+    }, 
+    HttpGet : function(opt, callback) {
+        const req = require('request');
+        req.get(opt.uri, opt, function(err, res, body) {
+            err ? callback(err, null, null) : callback(null, res.statusCode, body);
+        });
+    },
+    HttpPost : function(opt, data, callback) {
+        const req = require('request');
+        req.post(opt.uri, opt, function(err, res, body) {
+            err ? callback(err, null, null) : callback(null, res.statusCode, body);
+        }).form(data);
+    }, 
+    HttpPut : function(opt, data, callback) {
+        const req = require('request');
+        req.put(opt.uri, opt, function(err, res, body) {
+            err ? callback(err, null, null) : callback(null, res.statusCode, body);
+        }).form(data);
+    },
+    HttpDelete : function(opt, callback) {
+        const req = require('request');
+        req.delete(opt.uri, opt, function(err, res, body) {
+            err ? callback(err, null, null) : callback(null, res.statusCode, body);
+        });
+    },
+    Response : function(err, status, response, callback) {
+        if (err) {
+            if (err.code == "ECONNREFUSED") 
+                return callback(err, 503, "Сервер недоступен, повторите попытку позже");
+            return callback(err, status, response);
+        }
+        else {
+            response ? callback(err, status, response) : callback(err, status, null);
+        }
+    }
+}
