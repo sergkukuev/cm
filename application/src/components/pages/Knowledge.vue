e87c10ff2de147a85e67055915a4c6c0567010ca
Sergey <serg.kukuev@yandex.ru>
2018-07-11 16:48:04 +0300

    Добавлены страницы приложения

---
+++ application/src/components/pages/Knowledge.vue
@@ -0,0 +1,54 @@
+<template lang="html">
+  <div id="kns">
+    <table class="table">
+        <thead>
+          <tr>
+            <th>Знание</th>
+            <th>Категория</th>
+            <th>Подкатегория</th>
+            <th>Ранжирование оценок</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in kns" :key="item.id">
+            <td>{{item.name}}</td>
+            <td>{{item.category}}</td>
+            <td>{{item.sub_category}}</td>
+            <td>{{item.marks}}</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+import {API} from './../../router/api.js'
+
+export default {
+  name: 'knowledges',
+  data: function () {
+    return {
+      kns: [],
+      stPage: 0,
+      error: {}
+    }
+  },
+  methods: {
+    get_kns: function () {
+      let path = '/kns'
+      API.get(path).then((response) => {
+        this.kns = response.data
+        console.log(this.kns)
+      }, (err) => {
+        this.error = err
+      })
+    }
+  },
+  mounted: function () {
+    this.get_kns()
+  }
+}
+</script>
+
+<style lang="css">
+</style>
