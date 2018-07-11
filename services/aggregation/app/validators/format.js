86eb33795c0902cf9f40aa22332f752860e8030e
Sergey <serg.kukuev@yandex.ru>
2018-07-11 15:59:49 +0300

    Добавлена функция преобразования массива знаний

--- services/aggregation/app/validators/format.js
+++ services/aggregation/app/validators/format.js
@@ -1,9 +1,27 @@
 // Шаблоны для преобразования JSON перед отправкой результата запроса
 module.exports = {
+    // Преобразования ответа с проверкой на недоступность сервиса
     Data : function(err, res) {
         let result;
         err ? (err.code == "ECONNREFUSED" ? result = res : result = err) : 
             (res ? result = res : result = { status: "Error", description: "Ресурс не найден"});
+        return result;
+    },
+    // Формирование массива оценок 
+    MarkArray : function(what, from) {
+        let result = [];
+        for (let i = 0; i < from.length; i++) {
+            let flag = true;
+            for (let j = 0; j < what.length && flag ; j++) {
+                if (what[j].id_knowledge == from[i].id) {
+                    result.push(what[j].mark);
+                    flag = false;
+                }
+            }
+            if (flag) {
+                result.push(0);
+            }
+        }
         return result;
     }
 }
