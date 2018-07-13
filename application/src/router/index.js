29a9f5d7221e32d20e83e5b1d5117a6fa4994816
Sergey <serg.kukuev@yandex.ru>
2018-07-13 11:05:54 +0300

    Прописаны маршруты к знаниям

--- application/src/router/index.js
+++ application/src/router/index.js
@@ -2,23 +2,22 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // Pages
-
 import Auth from '@/components/pages/Authentication/Auth.vue'
 import Kn from '@/components/pages/Knowledge.vue'
+import KnList from '@/components/Knowledge/List.vue'
 
+// Global components
+import Navigate from '@/components/Navigate'
+Vue.component('app-navbar', Navigate)
+Vue.component('kn-list', KnList)
 Vue.use(Router)
 
+// Application routes
+const routes = [
+  { path: '/login', component: Auth },
+  { path: '/knowledges', components: { default: Kn, navbar: Navigate, list: KnList } }
+]
 export default new Router({
-  routes: [
-    {
-      path: '/login',
-      name: 'Auth',
-      component: Auth
-    },
-    {
-      path: '/knowledges',
-      name: 'Knowledge',
-      component: Kn
-    }
-  ]
+  routes,
+  linkActiveClass: 'is-active'
 })
