747fb25400608127d49befa3493d164d0f825c28
Sergey <serg.kukuev@yandex.ru>
2018-07-17 08:56:49 +0300

    Заужено меню

--- application/src/components/Navigate.vue
+++ application/src/components/Navigate.vue
@@ -1,15 +1,15 @@
 <template>
-  <v-footer height="auto" color="primary lighten-1">
+  <v-footer color="primary">
     <v-layout justify-space-between align-center row wrap >
       <v-flex>
-        <v-btn v-for="link in links" :key="link" color="white" flat round>
+        <v-btn v-for="link in links" :key="link" color="white" small flat round>
         {{ link }}
       </v-btn>
       </v-flex>
-      <v-flex xs2 md2>
+      <v-flex md2>
         <v-text-field v-model="user" dark disabled white--text append-icon="account_circle"></v-text-field>
       </v-flex>
-      <v-btn color="red white--text" round>
+      <v-btn color="red white--text" round small>
         Выйти
       </v-btn>
     </v-layout>
