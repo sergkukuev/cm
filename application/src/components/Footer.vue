e5db45655a485da08d0774a9abcd5ba9dd6b18f8
Sergey <serg.kukuev@yandex.ru>
2018-07-17 08:56:36 +0300

    Исправлен футер

--- application/src/components/Footer.vue
+++ application/src/components/Footer.vue
@@ -1,8 +1,10 @@
 <template>
-  <v-footer height="auto" color="primary lighten-1">
-    <v-flex primary lighten-2 py-3 text-xs-center white--text xs12>
-      &copy;{{ copyright }} — <strong>{{ company }}</strong>
-    </v-flex>
+  <v-footer color="primary" inset app>
+      <v-spacer></v-spacer>
+      <span class="white--text">
+        &copy;{{ copyright }} — <strong>{{ company }}</strong>
+      </span>
+      <v-spacer></v-spacer>
   </v-footer>
 </template>
 
