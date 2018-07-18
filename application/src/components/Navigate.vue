fcc6cd980fdd7df0bb87182d369ffa918562729a
Sergey <serg.kukuev@yandex.ru>
2018-07-18 08:57:33 +0300

    Добавлены действия кнопкам навигации

--- application/src/components/Navigate.vue
+++ application/src/components/Navigate.vue
@@ -1,13 +1,12 @@
 <template>
   <v-footer color="primary">
     <v-layout justify-space-between align-center row wrap >
-      <v-flex>
-        <v-btn v-for="link in links" :key="link" color="white" small flat round>
+      <v-btn v-for="link in links" :key="link" color="white" small flat round @click="my_location(link)">
         {{ link }}
       </v-btn>
-      </v-flex>
-      <v-flex md2>
-        <v-text-field v-model="user" dark disabled white--text append-icon="account_circle"></v-text-field>
+      <v-spacer></v-spacer>
+      <v-flex xs2 class="pl-5">
+        <v-text-field dark v-model="user" disabled white--text append-icon="account_circle"></v-text-field>
       </v-flex>
       <v-btn color="red white--text" round small>
         Выйти
@@ -24,7 +23,27 @@ export default {
       'Типовые работы',
       'Знания'
     ],
+    sites: [
+      '/cm',
+      '/work',
+      '/knowledge'
+    ],
     user: 'Admin Adminich'
-  })
+  }),
+  methods: {
+    my_location (link) {
+      let flExit = false
+      let index = -1
+      for (let i = 0; i < this.links.length && !flExit; i++) {
+        if (this.links[i] === item) {
+          flExit = true
+          index = i
+        }
+      }
+      if (index !== -1) {
+        window.location = 'http://localhost:8080/#' + this.sites[index]
+      }
+    }
+  }
 }
 </script>
