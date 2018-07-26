f64797d5ca9f87e9fb02a083ae647621e8b1e69c
Sergey <serg.kukuev@yandex.ru>
2018-07-26 15:06:26 +0300

    Добавлена валидация на отсутствие требуемых знаний

--- application/src/components/pages/works/KNeedList.vue
+++ application/src/components/pages/works/KNeedList.vue
@@ -1,40 +1,52 @@
 <template>
-  <v-layout column wrap>
-    <v-layout row>
-      <v-flex xs10>
-        <v-input class="body-2">Знание</v-input>
+  <div class="mx-2 elevation-1">
+    <v-layout row v-if="knowledges.length <= 0">
+      <v-spacer></v-spacer>
+      <v-flex xs4>
+        <v-input class="body-1">{{ no_data }}</v-input>
       </v-flex>
-      <v-flex xs2>
-        <v-input class="body-2">Оценки</v-input>
-      </v-flex>
+      <v-spacer></v-spacer>
     </v-layout>
+    <v-divider></v-divider>
     <div
-     class="body-1"
+      class="body-1"
       v-for="knowledge in knowledges"
       :key="knowledge.id">
       <v-layout row>
         <v-flex xs10>
-            <v-input class="body-1">{{ knowledge.name }}</v-input>
+          <v-input class="body-1 font-weight-light ml-4">{{ knowledge.name }}</v-input>
         </v-flex>
-        <v-flex xs2>
-          <v-tooltip top>
+        <v-flex xs1>
+          <v-tooltip top max-width="400px">
             <v-select
               slot="activator"
-              class="mt-1"
+              class="mt-1 mr-2"
               :items="ranks"
               v-model="knowledge.mark"
               persistent-hint
               return-object
               single-line>
             </v-select>
-            <span>Оценочные уровни знания: <br></span>
+            <span>Оценочные уровни:</span><br>
             <span v-for="(mark, i) in knowledge.marks" :key="i">{{i + 1}} - {{mark}}<br></span>
+          </v-tooltip>
+        </v-flex>
+        <v-flex xs1>
+          <v-tooltip bottom>
+            <v-icon
+              small
+              slot="activator"
+              @click="$emit('deleteMark', knowledges.indexOf(knowledge))"
+              class="mt-3 mr-4">
+              delete
+            </v-icon>
+            <span>Удалить оценку</span>
           </v-tooltip>
         </v-flex>
       </v-layout>
       <v-divider></v-divider>
     </div>
-  </v-layout>
+  </div>
   <!-- <v-data-table
     :items="knowledges"
     hide-headers
@@ -74,6 +86,7 @@ export default {
   data () {
     return {
       ranks: [1, 2, 3, 4],
+      no_data: 'Требуемые знания отсутствуют',
       headers: [
         { text: 'Знание', align: 'left', value: 'name' },
         { text: 'Оценка', sortable: false }
