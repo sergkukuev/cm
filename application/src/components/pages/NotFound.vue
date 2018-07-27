3080ac83397baaba317f0e900b2f3c2b2b35eacb
Sergey <serg.kukuev@yandex.ru>
2018-07-27 10:11:33 +0300

    Добавлена страница 404

---
+++ application/src/components/pages/NotFound.vue
@@ -0,0 +1,10 @@
+<template>
+  <v-layout column align-center fill-height>
+    <v-flex xs12>
+      <p class="display-1 text-xs-center">Страница не найдена (404)</p>
+    </v-flex>
+    <v-flex xs12>
+      <img src="./../../assets/images/not_found.png" width="90%" height="90%">
+    </v-flex>
+  </v-layout>
+</template>
--- application/src/router/index.js
+++ application/src/router/index.js
@@ -4,6 +4,7 @@ import Router from 'vue-router'
 // Pages
 import Kns from '@/components/pages/kns/Knowledge'
 import Work from '@/components/pages/works/Work'
+import NotFound from '@/components/pages/NotFound'
 
 // Global components
 import Navigate from '@/components/Navigate'
@@ -16,7 +17,9 @@ Vue.use(Router)
 // Application routes
 const routes = [
   { path: '/knowledge', component: Kns },
-  { path: '/work', component: Work }
+  { path: '/work', component: Work },
+  { path: '/404', name: '404', component: NotFound },
+  { path: '*', redirect: '/404' }
 ]
 export default new Router({
   routes,
