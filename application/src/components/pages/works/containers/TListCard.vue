785b993dbc7bf8b6f3633722726ea6aafced9d3e
Sergey <serg.kukuev@yandex.ru>
2018-07-30 13:15:55 +0300

    Список задач направления

---
+++ application/src/components/pages/works/containers/TListCard.vue
@@ -0,0 +1,54 @@
+<template>
+  <!-- Список задач для направления в элемент card -->
+  <v-card-text style="height: 300px">
+    <v-layout row wrap :class="frame" justify-center v-if="tasks.length == 0">
+      <span>{{ no_data }}</span>
+    </v-layout>
+    <v-layout row wrap :class="frame" v-else v-for="(task, i) in tasks" :key="i">
+      <v-flex xs10>
+        <span>
+          <span v-if="task.name == ''">Задача {{i + 1}}</span>
+          <span v-else>{{ task.name }}</span>
+          <br>
+          (Квалификация: {{ task.rank.value }}
+          <span class="hidden-xs-only">
+            , кол-во знаний: {{ task.need.length }}
+          </span>
+          )
+        </span>
+      </v-flex>
+      <v-flex xs2 align-start text-xs-right>
+        <v-tooltip bottom>
+          <v-icon slot="activator"
+            @click="$emit('editAction', i + 1)"
+            class="mx-1"
+          >
+            edit
+          </v-icon>
+          <span>Изменить</span>
+        </v-tooltip>
+        <v-tooltip bottom>
+          <v-icon slot="activator"
+            class="mx-1"
+            @click="$emit('deleteAction', i + 1)"
+          >
+            delete
+          </v-icon>
+          <span>Удалить</span>
+        </v-tooltip>
+      </v-flex>
+    </v-layout>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  props: ['tasks'],
+  data () {
+    return {
+      frame: 'background subheading font-weight-light mb-2 py-2 pl-3',
+      no_data: 'Задачи отсутствуют'
+    }
+  }
+}
+</script>
