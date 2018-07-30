4f428b67519874b374d11822a984a7cbfb42f43a
Sergey <serg.kukuev@yandex.ru>
2018-07-30 17:15:26 +0300

    Добавлена расшифровка квалификации

--- application/src/components/pages/works/containers/TListCard.vue
+++ application/src/components/pages/works/containers/TListCard.vue
@@ -10,7 +10,7 @@
           <span v-if="task.name == ''">Задача {{i + 1}}</span>
           <span v-else>{{ task.name }}</span>
           <br>
-          (Квалификация: {{ task.rank.value }}
+          (Квалификация: {{ find_rank(task.rank.value) }}
           <span class="hidden-xs-only">
             , кол-во знаний: {{ task.need.length }}
           </span>
@@ -47,7 +47,24 @@ export default {
   data () {
     return {
       frame: 'background subheading font-weight-light mb-2 py-2 pl-3',
-      no_data: 'Задачи отсутствуют'
+      no_data: 'Задачи отсутствуют',
+      ranks: [
+        { text: 'ведущий', value: 0 },
+        { text: '1 кат.', value: 1 },
+        { text: '2 кат.', value: 2 },
+        { text: '3 кат.', value: 3 },
+        { text: 'б/кат', value: 4 }
+      ]
+    }
+  },
+  methods: {
+    find_rank (value) {
+      for (let i = 0; i < this.ranks.length; i++) {
+        if (this.ranks[i].value === value) {
+          return this.ranks[i].text
+        }
+      }
+      return value // Не найдено
     }
   }
 }
