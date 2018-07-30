ce72a8dbf2edbd84558f05a4a70beb51d22afbc8
Sergey <serg.kukuev@yandex.ru>
2018-07-30 13:40:22 +0300

    Изменен список требуемых знаний

---
+++ application/src/components/pages/works/containers/KListCard.vue
@@ -0,0 +1,71 @@
+<template>
+  <!-- Список требуемых знаний для задачи в элемент card -->
+  <v-card-text style="height: 250px">
+    <v-layout row wrap :class="frame"
+      justify-center v-if="knowledges.length == 0"
+    >
+      <span>Задачи отсутствуют</span>
+    </v-layout>
+    <v-layout row wrap
+      v-else
+      v-for="knowledge in knowledges"
+      :key="knowledge.id"
+      :class="frame"
+    >
+      <v-flex d-flex xs12 md8 mt-2>
+        <span>{{ knowledge.name }}</span>
+      </v-flex>
+      <v-spacer></v-spacer>
+      <v-flex d-flex xs6 md2>
+        <v-tooltip top max-width="400px">
+          <!-- Выбор оценочного уровня знания -->
+          <v-select
+            slot="activator"
+            class="mt-1 mr-2 font-weight-light subheading"
+            :items="marks"
+            item-text="text"
+            item-value="value"
+            v-model="knowledge.mark"
+            persistent-hint
+            return-object
+            single-line
+          >
+          </v-select>
+          <span>Оценочные уровни:</span><br>
+          <span v-for="(mark, i) in knowledge.marks" :key="i">{{i + 1}} -
+            <strong>{{marks[i].text}}</strong>
+            - {{mark}}<br></span>
+        </v-tooltip>
+      </v-flex>
+      <v-flex d-flex xs6 md1 align-center text-xs-right>
+        <v-tooltip bottom>
+          <v-icon slot="activator"
+            class="mx-2"
+            @click="$emit('deleteMark', knowledges.indexOf(knowledge))"
+          >
+            delete
+          </v-icon>
+          <span>Удалить</span>
+        </v-tooltip>
+      </v-flex>
+    </v-layout>
+  </v-card-text>
+</template>
+
+<script>
+export default {
+  props: ['knowledges'],
+  data () {
+    return {
+      marks: [
+        { text: 'начальный', value: 1 },
+        { text: 'базовый', value: 2 },
+        { text: 'продвинутый', value: 3 },
+        { text: 'экспертный', value: 4 }
+      ],
+      frame: 'background subheading font-weight-light mb-2 py-2 pl-3',
+      no_data: 'Требуемые знания отсутствуют'
+    }
+  }
+}
+</script>
