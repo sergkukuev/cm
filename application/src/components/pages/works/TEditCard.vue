a878724a554d9b9ad221672a4e6074d0c8ce82d7
Sergey <serg.kukuev@yandex.ru>
2018-07-30 16:34:41 +0300

    Изменена карточка задачи

--- application/src/components/pages/works/TEditCard.vue
+++ application/src/components/pages/works/TEditCard.vue
@@ -1,89 +1,124 @@
 <template>
-  <v-layout row wrap>
-    <v-card style="width: 100%" class="elevation-3">
-      <v-card-title class="subheading">
-        Задача {{ index }}
-        <v-spacer></v-spacer>
-        <!-- <v-icon small class="mr-2">save</v-icon> -->
+  <v-card style="width: 100%" class="elevation-3">
+    <v-card-title class="title font-weight-medium accent" primary-title>
+      Задача {{ index }}
+      <v-spacer></v-spacer>
+      <!-- Действия с задачей: удалить и закрыть окно -->
+      <v-flex align-start text-xs-right>
         <v-tooltip bottom>
-          <v-icon slot="activator" small v-if="all > 1" @click="$emit('deleteAction')">delete</v-icon>
-          <span>Удалить задачу</span>
+          <v-icon slot="activator" @click="$emit('deleteAction', index)">delete</v-icon>
+          <span>Удалить</span>
         </v-tooltip>
-      </v-card-title>
-      <v-divider></v-divider>
-      <v-card-text>
-        <v-layout row wrap>
-          <v-flex xs7 class="mr-2">
-            <v-text-field
-              class="mt-1"
-              v-model="task.name"
-              label="Наименование задачи">
-            </v-text-field>
-          </v-flex>
-          <v-spacer></v-spacer>
-          <v-flex xs2>
-            <v-input class="subheading font-weight-light mt-2">Квалификация:</v-input>
-          </v-flex>
-          <v-flex xs2>
-            <v-select
-              class="mt-1"
-              v-model="task.rank"
-              label="Ранг"
-              :items="ranks"
-              item-text="text"
-              item-value="value"
-              persistent-hint
-              return-object
-              single-line>
-            </v-select>
-          </v-flex>
-          <v-flex>
-            <span class="subheading font-weight-regular">Требуемые знания:</span>
-            <v-tooltip bottom>
-              <v-btn slot="activator" @click="show_list = !show_list" icon small>
-                <v-icon>{{show_list ? 'keyboard_arrow_down' : 'keyboard_arrow_up'}}</v-icon>
-              </v-btn>
-              <span>{{show_list ? 'Скрыть' : 'Показать'}}</span>
-            </v-tooltip>
-          </v-flex>
+        <v-tooltip bottom>
+          <v-icon slot="activator" @click="$emit('closeAction')" class="ml-2">clear</v-icon>
+          <span>Закрыть</span>
+        </v-tooltip>
+      </v-flex>
+    </v-card-title>
+    <v-divider></v-divider>
+    <!-- Параметры задачи -->
+    <v-card-text>
+      <v-layout row wrap>
+        <v-flex d-flex xs12 md9>
+          <v-text-field
+            box
+            class="mx-1"
+            background-color="background"
+            v-model="task.name"
+            label="Наименование задачи"
+          >
+          </v-text-field>
+        </v-flex>
+        <v-flex d-flex xs12 md3>
+          <v-select
+            class="mx-1"
+            v-model="task.rank"
+            label="Квалификация"
+            background-color="background"
+            :items="ranks"
+            item-text="text"
+            item-value="value"
+            persistent-hint
+            return-object
+            box
+          >
+          </v-select>
+        </v-flex>
+        <!-- Заголовок списка требуемых знаний -->
+        <v-flex d-flex xs12 sm7 mt-4>
+          <span class="title font-weight-regular">Требуемые знания:</span>
+        </v-flex>
+        <v-flex xs1 sm1 text-xs-right>
           <v-tooltip bottom>
             <v-icon
+              class="mt-4 mb-0 mr-2"
               slot="activator"
-              class="mt-2"
               @click="task.need = []"
-              v-if="task.need.length != 0">clear</v-icon>
+              v-if="task.need.length != 0"
+            >
+              clear
+            </v-icon>
             <span>Очистить список</span>
           </v-tooltip>
+        </v-flex>
+        <!-- Диалоговое окно-селектор знаний -->
+        <v-flex d-flex xs11 sm4>
           <v-dialog v-model="dialog" max-width="800px">
             <v-btn
+              block
               slot="activator"
-              class="accent text--primary font-weight-regular"
-              small>
+              class="accent text--primary font-weight-regular mt-3 mb-0"
+            >
               Выбрать
             </v-btn>
             <kns-selector
               :open="dialog"
               :exists="task.need"
               @selectedAction="selected_action"
-              @cancelAction="dialog = false">
+              @cancelAction="dialog = false"
+            >
             </kns-selector>
           </v-dialog>
-          <v-flex xs12>
-            <kns-need-list
-              v-show="show_list"
-              :knowledges="task.need"
-              @deleteMark="delete_mark">
-            </kns-need-list>
-          </v-flex>
-        </v-layout>
-      </v-card-text>
-    </v-card>
-  </v-layout>
+        </v-flex>
+      </v-layout>
+    </v-card-text>
+    <v-divider></v-divider>
+    <!-- Список выбранных требуемых знаний -->
+    <kns-need-list :knowledges="task.need" @deleteMark="delete_mark">
+    </kns-need-list>
+    <v-divider></v-divider>
+    <!-- Кнопки переключения между задачами -->
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-tooltip bottom>
+        <v-btn slot="activator"
+          :disabled="index <= 1"
+          icon
+          large
+          @click="$emit('prevAction')"
+        >
+          <v-icon>keyboard_arrow_left</v-icon>
+        </v-btn>
+        <span>Предыдущая</span>
+      </v-tooltip>
+      <v-tooltip bottom>
+        <v-btn slot="activator"
+          :disabled="index >= all"
+          icon
+          large
+          @click="$emit('nextAction')"
+        >
+          <v-icon>keyboard_arrow_right</v-icon>
+        </v-btn>
+        <span>Следующая</span>
+      </v-tooltip>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-import KSelector from './KSelector'
-import KNeedList from './KNeedList'
+import KSelector from './containers/KSelector'
+import KNeedList from './containers/KListCard'
 
 export default {
   props: ['index', 'all', 'task'],
@@ -100,8 +135,7 @@ export default {
         { text: '3 кат.', value: 3 },
         { text: 'б/кат', value: 4 }
       ],
-      dialog: false,
-      show_list: false
+      dialog: false
     }
   },
   methods: {
