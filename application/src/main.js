176987981844dae19e31ebed4e68fca1dda85ec8
Sergey <serg.kukuev@yandex.ru>
2018-09-05 14:16:12 +0300

    Вынос из папки маршрутизатор страниц

--- application/src/main.js
+++ application/src/main.js
@@ -1,8 +1,9 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+// import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue'
 import App from './App'
-import router from './router'
+import router from './router.js'
 
 import VueCookie from 'vue-cookie'
 import Vuetify from 'vuetify'
--- application/src/router.js
+++ application/src/router.js
@@ -2,16 +2,16 @@ import Vue from 'vue'
 import Router from 'vue-router'
 
 // Pages
-import Kns from '@/components/pages/kns/Knowledge'
-import Work from '@/components/pages/works/Work'
-import NotFound from '@/components/pages/NotFound'
+import Kns from '@/pages/Knowledges'
+import Work from '@/pages/Works'
+import NotFound from '@/pages/NotFound'
 
 Vue.use(Router)
 
 // Application routes
 const routes = [
-  { path: '/knowledge', component: Kns },
-  { path: '/work', component: Work },
+  { path: '/knowledges', component: Kns },
+  { path: '/works', component: Work },
   { path: '/404', name: '404', component: NotFound },
   { path: '*', redirect: '/404' }
 ]
