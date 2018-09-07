e25ec44b0ded5f7b53a97e1585c513bbdeb026cb
Sergey <serg.kukuev@yandex.ru>
2018-09-07 15:08:09 +0300

    Возвращена старая цветовая расцветка

--- application/src/main.js
+++ application/src/main.js
@@ -16,7 +16,7 @@ Vue.use(VueCookie)
 Vue.use(Vuetify, {
   theme: {
     primary: colors.grey.darken3,
-    secondary: colors.grey.lighten1,
+    secondary: colors.amber.lighten1,
     accent: colors.amber.lighten4,
     background: colors.grey.lighten4
   }
