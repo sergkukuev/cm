e2fd79d8b954b261e5d7bb109bca9b0f2900d0e5
Sergey <serg.kukuev@yandex.ru>
2018-09-12 16:14:25 +0300

    Изменена обработка ошибок. Внесены визуальные правки

--- services/auth/app/passport/index.js
+++ services/auth/app/passport/index.js
@@ -1,41 +1,59 @@
 const   basic       = require('basic-auth'),
-        strategy    = require('./strategy') ;
+        strategy    = require('./strategy'),
+        log         = require('./../../config/log')(module);
 
 const basicType  = /basic/i;
 const bearerType = /bearer/i;
 
-
 module.exports = {
-    checkServiceAuthorization : function(header_authorization, callback) {
+    // Проверка сервисной авторизации
+    CheckServiceAuth : function(header_authorization, callback) {
+        log.info('START - Checker service authorization');
         const type = basicType.test(header_authorization);
-        if (type) {
-            return checkBasicAuthorization(header_authorization, callback);
-        } else if (bearerType.test(header_authorization)) {
-            return checkBearerAuthorization(header_authorization, callback);
-        }
-        return callback('Unknown authorization type', 400, null);
+        if (type)
+            return checkBasicAuth(header_authorization, callback);
+        else if (bearerType.test(header_authorization))
+            return checkBearerAuth(header_authorization, callback);
+        return callback(new Error('Unknown authorization type'), 400, null);
+    },
+    // Проверка авторизации пользователя
+    CheckUserByBearer : function(header_text , callback) {
+        log.info('START - Checker user authorization by bearer token');
+        if (!bearerType.test(header_text))
+            return callback(new Error('Isn\'t Bearer token'), 400, null);
+        const token = getBearer(header_text);
+        return strategy.СheckUserByAToken(token, function(err, status, user) {
+            if (err)
+                return callback(err, status);
+            if (status || !user)
+                return callback(new Error('User not found'), status);
+            return callback(null, status, user);
+        });
     },
     /**
     * @param {Object} data - data
     */
-    getUserCode : function (data, callback) {
-        const validator = checkResponseType(data.responseType, 'code');
-        if (!validator) {
-            return callback('Invalid responseType', 400, null);
-        }
-        return strategy.checkServiceById(data.appId, function (err, status, response) {
-            if (err) {
+    // Получение кода пользователя для авторизации
+    GetUserCode : function (data, callback) {
+        log.info('START - Get user code for auth');
+        const validator = checkResType(data.response_type, 'code');
+        if (!validator)
+            return callback(new Error('Invalid response type'), 400, null);
+
+        return strategy.CheckServiceById(data.appId, function (err, status, response) {
+            if (err)
                 return callback(err, status, response);
-            } else if (!response) {
+            else if (!response)
                 return callback(err, status, null);
-            } else
-                return strategy.getUserCode(data.login, data.password, function(err, status, code){
-                    return callback(err, status, code);
-                });
+            return strategy.GetUserCode(data.login, data.password, function(err, status, code){
+                return callback(err, status, code);
+            });
         });
     },
-    setUserTokenByCode : function(code, callback) {
-        return strategy.createTokenForUser(code, function(err, status, scope) {
+    // Пересоздание токенов пользователя по коду
+    SetUTokenByCode : function(code, callback) {
+        log.info('START - Set user tokens by code');
+        return strategy.UTokenByCode(code, function(err, status, scope) {
             if (err)
                 return callback(err, status, null);
             if (!scope)
@@ -43,8 +61,10 @@ module.exports = {
             return callback(null, null, scope);
         });
     },
-    setUserTokenByPass : function(data, callback) {
-        return strategy.createTokenForUserByPass(data, function(err, status, scope) {
+    // Пересоздание токенов пользователя по паролю
+    SetUTokenByPass : function(data, callback) {
+        log.info('START - Set user tokens by password');
+        return strategy.UTokenByPass(data, function(err, status, scope) {
             if (err)
                 return callback(err, status, null);
             if (!scope)
@@ -52,43 +72,28 @@ module.exports = {
             return callback(null, null, scope);
         });
     },
-    setUserTokenByToken : function(token, callback) {
-        return strategy.createTokenForUserByToken(token, function(err, status, scope) {
+    // Пересоздание токенов пользователя по токену
+    SetUTokenByToken : function(token, callback) {
+        log.info('START - Set user tokens by token');
+        return strategy.UTokenByToken(token, function(err, status, scope) {
             if (err)
                 return callback(err, status, null);
             if (!scope)
                 return callback(null, status, null);
             return callback(null, null, scope);
         });
-    },
-    checkUserByBearer : function(header_text , callback) {
-        if (!bearerType.test(header_text))
-            return callback('Is not Bearer token', 400, null);
-        const token = getBearer(header_text);
-        return strategy.checkUserByAccessToken(token, function(err, status, user) {
-            if (err)
-                return callback(err, status);
-            if (status || !user)
-                return callback('User not found', status);
-            return callback(null, status, user);
-        });
-    }   
+    }
 }
 
-function checkResponseType(type, needed) {
-    if (type === needed)
-        return true;
-    return false;
-}
-
-function checkBasicAuthorization(header_authorization, callback) {
+// Проверка авторизации по appId и appSecret
+function checkBasicAuth(header_authorization, callback) {
     const service = basic.parse(header_authorization);
-    return strategy.checkService(service.name, service.pass, function(err, status, application) {
+    return strategy.CheckService(service.name, service.pass, function(err, status, application) {
         if (err)
             return callback(err, status, null);
         if (!application)
             return callback(null, status, null);
-        return strategy.setNewAccessToken(application, function(err, status, scope) {
+        return strategy.SetNewAToken(application, function(err, status, scope) {
             if (err)
                 return callback(err, status, null);
             if (!scope)
@@ -98,15 +103,24 @@ function checkBasicAuthorization(header_authorization, callback) {
     });
 }
 
-function checkBearerAuthorization(header_authorization, callback) {
+// Проверка авторизации по токену
+function checkBearerAuth(header_authorization, callback) {
     const serviceToken = getBearer(header_authorization);
-    return strategy.checkServiceAccessToken(serviceToken, function(err, status, result) {
+    return strategy.CheckServiceAToken(serviceToken, function(err, status, result) {
         if (err)
             return callback(err, status, null);
         return callback(null, status, result);
     });
 }
 
+// Проверка на совпадение с нужным типом
+function checkResType(type, needed) {
+    if (type === needed)
+        return true;
+    return false;
+}
+
+// Получить токен
 function getBearer(token) {
     token = String(token);
     token = token.slice(7);
