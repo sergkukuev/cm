aa67d835938f65a5b2b39e1ed0011f04d04d9ccb
Sergey <serg.kukuev@yandex.ru>
2018-09-19 09:37:17 +0300

    Исправлен последний обработчик запросов. Произведена чистка кода

--- services/matrix/config/express.js
+++ services/matrix/config/express.js
@@ -4,56 +4,73 @@ var express         = require('express'),
     cookieParser    = require('cookie-parser'),
     bodyParser      = require('body-parser'),
     compress        = require('compression'),
-    methodOverride  = require('method-override');
+    methodOverride  = require('method-override'),
+    log             = require('./log')(module),
+    verify          = require('./../app/models/client').VerifyClient;
 
 module.exports = function(app, config) {
-  var env = process.env.NODE_ENV || 'development';
-  app.locals.ENV = env;
-  app.locals.ENV_DEVELOPMENT = env == 'development';
-  
-  // app.use(favicon(config.root + '/public/img/favicon.ico'));
-  app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({
-    extended: true
-  }));
-  app.use(cookieParser());
-  app.use(compress());
-  app.use(express.static(config.root + '/public'));
-  app.use(methodOverride());
+    var env = process.env.NODE_ENV || 'development';
+    app.locals.ENV = env;
+    app.locals.ENV_DEVELOPMENT = env == 'development';
 
-  var models = glob.sync(config.root + '/app/models/*.js');
-  models.forEach(function (model) {
-    require(model);
-  });
-
-  var routes = glob.sync(config.root + '/app/routes/*.js');
-  routes.forEach(function (route) {
-    require(route)(app);
-  });
-
-  app.use(function (req, res, next) {
-    var err = new Error('Route not found');
-    err.status = 404;
-    next(err);
-  });
-  
-  if(app.get('env') === 'development'){
-    app.use(function (err, req, res, next) {
-      res.status(err.status || 500);
-      res.send({
-        status: 'Error',
-        message: err.message
-      });
+    // app.use(favicon(config.root + '/public/img/favicon.ico'));
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(cookieParser());
+    app.use(compress());
+    app.use(express.static(config.root + '/public'));
+    app.use(methodOverride());
+    // Установка моделей
+    var models = glob.sync(config.root + '/app/models/*.js');
+    models.forEach(function (model) {
+        require(model);
     });
-  }
+    // Установка путей
+    var routes = glob.sync(config.root + '/app/routes/*.js');
+    routes.forEach(function (route) {
+        require(route)(app);
+    });
 
-  app.use(function (err, req, res, next) {
-    res.status(err.status || 500);
-      res.send({
-        status: 'Error',
-        message: err.message
-      });
-  });
+    verify('aggregator', 'aggr_id', 'aggr_secret', true);   // Проверка наличия записи о клиенте в бд
 
-  return app;
+    // 404
+    app.use(function(req, res, next) {
+        var err = new Error('Route not found');
+        err.status = 404;
+        return next(err);
+    });
+
+    // Завершающий обработчик запросов
+    app.use(function(err, req, res, next) {
+        let item = {
+            status: err.name || 'Error',
+            code: err.status || 500,
+            message: err.message || 'Unknown error',
+            service: err.service || 'undefined'
+        };
+        log.error(`${item.message} - ${item.code} - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        log.debug(err.stack);
+        // Обработка уникальных случаев повторной инициализации функций:
+        // res.status()
+        if (typeof(res.status) != 'function') {
+            log.warn('Attempt to reinitialize an object \'status\'');
+            res.status = item.code; 
+        } else {
+            res.status(item.code);
+        }
+        // Заголовок не пустой, значит запрос уже был отправлен где-то раньше
+        if (res._header != null)
+            log.warn('Attempt to reinitialize an object \'send\'');
+        else {
+            // res.send()
+            if (typeof(res.status) != 'function') {
+                log.error('Attempt to reinitialize an object \'send\'');
+            }
+            res.send(item);
+        }
+    });
+
+    return app;
 };
--- services/matrix/config/log.js
+++ services/matrix/config/log.js
@@ -1,13 +1,14 @@
 var winston = require('winston');
 
 function getLogger(module) {
-    var path = module.filename.split('/').slice(-2).join('/');
+    var path = module.filename.split('/').slice(-2).join('/'); //отобразим метку с именем файла, который выводит сообщение
 
     return new winston.Logger({
         transports : [
             new winston.transports.Console({
+                timestamp: true,
                 colorize: true,
-                level: 'debug',
+                level: 'debug', // Уровни вывода: error, warn, info, verbose, debug, silly
                 label: path
             })
         ]
