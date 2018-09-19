2e6e0e1f8aebdc9a79f822cebe2dd12dd87194c8
Sergey <serg.kukuev@yandex.ru>
2018-09-19 14:29:11 +0300

    Приведено в божеский вид, добавлено логирование

--- services/aggregation/app/routes/knowledges.js
+++ services/aggregation/app/routes/knowledges.js
@@ -2,7 +2,7 @@
 var express = require('express'),
     router  = express.Router(),
     crd     = require('./../coordinators'),
-    format  = require('./../validators/format');
+    log     = require('./../../config/log')(module);
 
 module.exports = function(app) { 
     app.use('/api/kns', router);
@@ -10,60 +10,90 @@ module.exports = function(app) {
 
 // Создание знания
 router.post('/create', function(req, res, next) {
+    log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
     const data = {
         name: req.body.name, 
         ctgr: req.body.ctgr,
         sctgr: req.body.sctgr,
         marks: req.body.marks
     };
-    crd.CreateKn(data, function(err, st, response) {
-        err ? res.status(st).send(format.T(st, err)) : 
-            res.status(st).send(format.Data(response));
+    crd.CreateKn(data, function(err, status, response) {
+        if (err) {
+            err.status = err.status || status;
+            return next(err);
+        }
+        log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        return res.status(status).send(response);
     });
 });
 
 // Получение всех знаний из базы
 router.get('/', function(req, res, next) {
-    crd.GetKns(req.query.page, req.query.count, function(err, st, response){
-        err ? res.status(st).send(format.T(st, err)) : 
-            res.status(st).send(format.Data(response));
+    log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    crd.GetKns(req.query.page, req.query.count, function(err, status, response) {
+        if (err) {
+            err.status = err.status || status;
+            return next(err);
+        }
+        log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        return res.status(status).send(response);
     });
 });
 
 // Получение знания по id
 router.get('/:id', function(req, res, next) {
-    crd.GetKnById(req.params.id, function(err, st, response) {
-        err ? res.status(st).send(format.T(st, err)) : 
-            res.status(st).send(format.Data(response));
+    log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    crd.GetKnById(req.params.id, function(err, status, response) {
+        if (err) {
+            err.status = err.status || status;
+            return next(err);
+        }
+        log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        return res.status(status).send(response);
     });
 });
 
 // Обновление знания по id
 router.put('/:id', function(req, res, next) {
+    log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
     let data = {
         name: req.body.name,
         ctgr: req.body.ctgr, 
         sctgr: req.body.sctgr,
         marks: req.body.marks
     };
-    crd.UpdateKn(req.params.id, data, function(err, st, response) {
-        err ? res.status(st).send(format.T(st, err)) : 
-            res.status(st).send(format.Data(response));
+    crd.UpdateKn(req.params.id, data, function(err, status, response) {
+        if (err) {
+            err.status = err.status || status;
+            return next(err);
+        }
+        log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        return res.status(status).send(response);
     });
 });
 
 // Удаление всех знаний из базы
 router.delete('/', function(req, res, next) {
-    crd.DeleteKns(function(err, st, response) {
-        err ? res.status(st).send(format.T(st, err)) : 
-            res.status(st).send(format.Data(response));
+    log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    crd.DeleteKns(function(err, status, response) {
+        if (err) {
+            err.status = err.status || status;
+            return next(err);
+        }
+        log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        return res.status(status).send(response);
     });
 });
 
 // Удаление знания по id
 router.delete('/:id', function(req, res, next) {
-    crd.DeleteKnById(req.params.id, function(err, st, response) {
-        err ? res.status(st).send(format.T(st, err)) : 
-            res.status(st).send(format.Data(response));
+    log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+    crd.DeleteKnById(req.params.id, function(err, status, response) {
+        if (err) {
+            err.status = err.status || status;
+            return next(err);
+        }
+        log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
+        return res.status(status).send(response);
     });
 });
