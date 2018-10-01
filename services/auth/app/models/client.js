c152dd9fc3e1de241e7fa5f6c1779f8a5c9c8ec9
Sergey <serg.kukuev@yandex.ru>
2018-10-01 14:34:38 +0300

    #4 Добавлены функции для взаимодействия с бд клиентов

--- services/auth/app/models/client.js
+++ services/auth/app/models/client.js
@@ -1,8 +1,7 @@
-const   mongoose    = require('mongoose'),
-        log         = require('./../../config/log')(module);
+const mongoose = require('mongoose'),
+      Schema   = mongoose.Schema;
 
-const Schema  = mongoose.Schema;
-
+// Модель клиента
 const Client = new Schema({
     name: {
         type: String, 
@@ -19,19 +18,79 @@ const Client = new Schema({
     }
 });
 
+// Получение клиента по appId
+Client.statics.GetByApp = function(id, callback) {
+    return findByData({ appId: id }, callback);
+}
+
+// Получение клиента по appId и appSecret
+Client.statics.GetByAppAndSecret = function(id, secret, callback) {
+    let data = {
+        appId: id,
+        appSecret: secret
+    };
+    return findByData(data, callback);
+}
+
+// Получение клиента по id
+Client.statics.GetById = function(id, callback) {
+    this.findById(id, function(err, app) {
+        if (err) {
+            return callback(err, null);
+        } else if (!app) {
+            let err = new Error('Wrong access token');
+            err.name = 'ServiceTokenError';
+            return callback(err, null);
+        }
+        return callback(null, app);
+    });
+}
+
+// Удаление всех клиентов из базы
+Client.statics.Clear = function(callback) {
+    this.remove({}, function(err, result) {
+        if (err) 
+            return callback(err, null);
+        else
+            result ? callback(null, result) : callback(new Error('Deleting failed'), null);
+    });
+}
+
 mongoose.model('Client', Client);
-var client = mongoose.model('Client');
+
+const log = require('./../../config/log')(module);
+var ClientModel = mongoose.model('Client');
+module.exports.model = ClientModel;
+
+// Поиск клиента по информации
+function findByData(data, callback) {
+    ClientModel.findOne(data, function(err, app) {
+        if (err) {
+            return callback(err, null);
+        } else if (!app) {
+            let err = new Error('Client with this data not found');
+            err.name = 'ServiceTokenError';
+            return callback(err, null);
+        }
+        return callback(null, app);
+    });
+}
 
 // Проверка клиента-агрегатора в базе
 module.exports.VerifyClient = function(name, appId, appSecret, created) {
-    client.findOne({name : name}, function(err, app) {
+    let data = {
+        name: name,
+        appId: appId,
+        appSecret: appSecret
+    };
+    ClientModel.findOne(data, function(err, app) {
         if (err) {
             log.error(`${err.status || 500} - ${err.message}`);
             log.debug(err.stack);
         }
         // Создать клиент
         if (!app && created) {
-            let aggregator = new client({
+            let aggregator = new ClientModel({
                 name        : name,
                 appId       : appId,
                 appSecret   : appSecret
@@ -49,5 +108,3 @@ module.exports.VerifyClient = function(name, appId, appSecret, created) {
         log.info('Client \'' + name + '\' exists');
     });
 }
-
-module.exports.model = client;
