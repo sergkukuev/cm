dc2e24d593cef4c04fb473682a7616845929745f
Sergey <serg.kukuev@yandex.ru>
2018-10-03 10:25:41 +0300

    Исправлена авторизация и обработка ошибок

--- services/auth/app/routes/auth.js
+++ services/auth/app/routes/auth.js
@@ -16,19 +16,24 @@ module.exports = (app) => {
 // Получить код авторизации
 router.put('/code', function(req, res, next) {
     log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-    passport.ServiceAuth(req.headers['authorization'], function(err, scope) {
-        if (err)
+    passport.ServiceAuth(req.headers, function(err, scope) {
+        if (err) {
             return next(err);
+        }
         const data = {
             login: validator.Validity(req.body.login),
             password: validator.Validity(req.body.password),
             response_type: validator.ResponseType(req.body.grant_type)
         };
-        if (!data.login || !data.pass)
-            return next(TError(null, true, 'Login or password is undefined', 400, scope));
+        if (!data.login || !data.password) {
+            return next(TError(null, true, 'Invalid parameter "login" or "password"', 400, scope));
+        } else if (!data.response_type) {
+            return next(TError(null, true, 'Invalid parameter "grant_type"', 400, scope));
+        }
         passport.GetUserCode(data, function(err, code) {
-            if (err)
+            if (err) {
                 return next(err);
+            }
             log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
             return res.status(202).send(TData({ code: code }, scope));
         });
@@ -38,9 +43,10 @@ router.put('/code', function(req, res, next) {
 // Получить токен авторизации
 router.post('/token', function(req, res, next) {
     log.info(`START - ${req.originalUrl} - ${req.method} - ${req.ip}`);
-    passport.ServiceAuth(req.headers['authorization'], function(err, scope) {
-        if (err)
+    passport.ServiceAuth(req.headers, function(err, scope) {
+        if (err) {
             return next(err);
+        }
         let type = validator.ResponseType(req.body.grant_type);
         if (type === 'code') {
             return codeAuthorization(req, res, next, scope);
@@ -49,7 +55,7 @@ router.post('/token', function(req, res, next) {
         } else if (type === 'password') {
             return passAuthorization(req, res, next, scope);
         } else {
-            return next(TError(null, true, 'Parameter "grant_type" is undefined', 400, scope));
+            return next(TError(null, true, 'Invalid parameter "grant_type"', 400, scope));
         }
     });
 });
@@ -59,14 +65,13 @@ router.post('/token', function(req, res, next) {
 function codeAuthorization(req, res, next, service_scope) {
     log.info('Code authorization');
     const code = validator.Validity(req.body.code);
-    if (!code)
-        return next(TError(null, true, 'Login or password is undefined', 400, service_scope));
+    if (!code) {
+        return next(TError(null, true, 'Invalid parameter "code"', 400, service_scope));
+    }
     return passport.SetUTokenByCode(code, function(err, status, user_scope) {
-        if (err) 
-            return next(TError(err, true, err.status || status, scope));
-        if (!user_scope)
-            return next(TError(null, true, 'User for this password and login is not found', status, service_scope));
-
+        if (err) {
+            return next(TError(err, true, err.status || status, service_scope));
+        }
         log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
         return res.status(200).send(TData(user_scope, service_scope));
     });
@@ -77,16 +82,14 @@ function passAuthorization(req, res, next, service_scope) {
     log.info('Password authorization');
     const data = {
         login: validator.Validity(req.body.login), 
-        pass: validator.Validity(req.body.password)
+        password: validator.Validity(req.body.password)
     };
-    if (!data.login || !data.pass)
-        return next(TError(null, true, 'Login or password is undefined', 400, service_scope));
+    if (!data.login || !data.password)
+        return next(TError(null, true, 'Invalid parameters "login" or "password"', 400, service_scope));
     return passport.SetUTokenByPass(data, function(err, status, user_scope) {
-        if (err)
-            return next(TError(err, true, err.status || status, scope));
-        if (!user_scope)
-            return next(TError(null, true, 'User for this password and login is not found', status, service_scope));
-        
+        if (err) {
+            return next(TError(err, true, err.status || status, service_scope));
+        }
         log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
         return res.status(200).send(TData(user_scope, service_scope));
     });
@@ -96,14 +99,13 @@ function passAuthorization(req, res, next, service_scope) {
 function rTokenAuthorization(req, res, next, service_scope) {
     log.info('Token authorization');
     const token = validator.Validity(req.body.refresh_token);
-    if (!token)
-        return next(TError(null, true, 'Token is undefined', 400, service_scope));
+    if (!token) {
+        return next(TError(null, true, 'Invalid parameter "refresh_token"', 400, service_scope));
+    }
     return passport.SetUTokenByToken(token, function(err, status, user_scope) {
-        if (err)
-            return next(TError(err, true, err.status || status, scope));
-        if (!user_scope)
-            return next(TError(null, true, 'User for this password and login is not found', status, service_scope));
-        
+        if (err) {
+            return next(TError(err, true, err.status || status, service_scope));
+        }
         log.info(`SUCCESS - ${req.originalUrl} - ${req.method} - ${req.ip}`);
         return res.status(200).send(TData(user_scope, service_scope));
     });
